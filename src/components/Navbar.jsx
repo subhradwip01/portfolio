f1dfdd93cd8b860c010afd40b5c77629e6d86a34
subhradwip01 <subhradwip01@gmail.com>
2022-08-17 21:52:57 +0530

    Navbar created
    Signed-off-by: subhradwip01 <subhradwip01@gmail.com>

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,52 @@
+import { AppBar, Toolbar, Typography } from '@mui/material'
+import React from 'react'
+import { styled } from "@mui/material/styles"
+const NavBar=styled(AppBar)(({theme})=>({
+    backgroundColor: theme.primary, 
+    padding: theme.spacing(1.4),
+    postion:"sticky",
+    zIndex: 1100,
+    boxShadow:"none"
+}))
+
+const Menu=styled('ul')(()=>({
+    display:"flex",
+    listStyle:"none"
+}))
+
+const MenuItem=styled('li')(({theme})=>({
+    paddingRight:theme.spacing(3),
+}))
+
+const MenuItemLink=styled('a')(({theme})=>({
+   textDecoration:"none",
+   color:theme.palette.grey.dark,
+   fontSize:theme.spacing(2.3),
+   fontWeight:theme.typography.fontWeightRegular,
+   "&:hover":{
+    color:theme.palette.secondary.main
+   } 
+}))
+
+const Navbar = () => {
+  return (
+   <NavBar>
+    <Toolbar sx={{
+        justifyContent:"space-between"
+    }}>
+        <Typography variant="h6">
+            Subhradwip
+        </Typography>
+        <Menu>
+        {['Home','About','Skills','Work','Experince'].map((item=>(
+            <MenuItem key={`link-#${item}`}>
+               <MenuItemLink href={`#${item}`}>{item}</MenuItemLink> 
+            </MenuItem>
+        )))}
+        </Menu>
+    </Toolbar>
+   </NavBar>
+  )
+}
+
+export default Navbar
