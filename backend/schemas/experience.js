a481090e57b1fa247679b8dfee7857493da19bd5
subhradwi01 <subhradwi01@gmail.com>
2023-03-15 18:35:28 +0530

    Work upadate:timeline added
    Signed-off-by: subhradwi01 <subhradwi01@gmail.com>

--- backend/schemas/experience.js
+++ backend/schemas/experience.js
@@ -1,64 +1,63 @@
 export default {
-    name: "works",
-    title: "Works",
-    type: "document",
-    fields: [
-      {
-        name: "company",
-        title: "Company",
-        type: "string",
+  name: "works",
+  title: "Works",
+  type: "document",
+  fields: [
+    {
+      name: "company",
+      title: "Company",
+      type: "string",
+    },
+    {
+      name: "logo",
+      title: "Logo",
+      type: "image",
+      options: {
+        hotspot: true,
       },
-      {
-        name: "logo",
-        title: "Logo",
-        type: "image",
-        options: {
-          hotspot: true,
+    },
+    {
+      name: "link",
+      title: "Link",
+      type: "string",
+    },
+    {
+      name: "role",
+      title: "Role",
+      type: "string",
+    },
+    {
+      name: "startDate",
+      title: "startDate",
+      type: "date",
+      options: {
+        dateFormat: "YYYY-MM",
+        calendarTodayLabel: "Today",
+      },
+    },
+
+    {
+      name: "endDate",
+      title: "endtDate",
+      type: "date",
+      options: {
+        dateFormat: "YYYY-MM",
+        calendarTodayLabel: "Today",
+      },
+    },
+
+    {
+      name: "workDone",
+
+      title: "WorkDone",
+      type: "array",
+      of: [
+        {
+          name: "work",
+          title: "Work",
+          type: "string",
         },
-      },
-      {
-        name: "link",
-        title: "Link",
-        type: "string",
-      },
-      {
-        name: "role",
-        title: "Role",
-        type: "string",
-      },
-      {
-        name: "startDate",
-        title: "startDate",
-        type: "date",
-        options: {
-          dateFormat: "YYYY-MM",
-          calendarTodayLabel: "Today",
-        },
-      },
-  
-      {
-        name: "endDate",
-        title: "endtDate",
-        type: "date",
-        options: {
-          dateFormat: "YYYY-MM",
-          calendarTodayLabel: "Today",
-        },
-      },
-  
-      {
-        name: "workDone",
-  
-        title: "WorkDone",
-        type: "array",
-        of: [
-          {
-            name: "work",
-            title: "Work",
-            type: "string",
-          },
-        ],
-      },
-    ],
-  };
-  
+      ],
+    },
+  ],
+};
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,60 +1,150 @@
-import { Typography,Grid, Link } from "@mui/material";
-import React,{useState,useEffect} from "react";
-import Card from "@mui/material/Card";
+import { Typography, Grid, Link } from "@mui/material";
+import React, { useState, useEffect } from "react";
 import CardHeader from "@mui/material/CardHeader";
 import CardContent from "@mui/material/CardContent";
 import { Box } from "@mui/system";
-import {motion} from "framer-motion"
+import { motion } from "framer-motion";
 import { urlFor, client } from "../client";
-import LaunchIcon from '@mui/icons-material/Launch';
+import LaunchIcon from "@mui/icons-material/Launch";
+import { styled } from "@mui/material/styles";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+
+const WorkList=styled('ul')(({ theme }) => ({
+  padding:"0px",
+  marginLeft:"14px"
+}));
+const WorkListItem=styled('li')(({ theme }) => ({
+    color: theme.palette.grey.dark
+}));
+
+
+
+
+const WorkCard = ({ work }) => {
+  return (
+    <VerticalTimelineElement
+      contentStyle={{
+        color: "#1d1836",
+        background: "#fff",
+      }}
+      contentArrowStyle={{ borderRight: "7px solid #ffff" }}
+      date={work.date}
+      iconStyle={{ background: "white" }}
+      icon={
+        <Box
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          width="100%"
+          height="100%"
+        >
+          <img
+            src={urlFor(work.logo)}
+            alt={work.company}
+            style={{
+              width: "60%",
+              height: "60%",
+              objectFit: "contain",
+            }}
+          />
+        </Box>
+      }
+    >
+      <Box hover sx={{ maxWidth: 600, marginBottom: 2, cursor: "pointer" }}>
+        <CardHeader
+          avatar={<img src={urlFor(work.logo)} style={{ width: 70 }} />}
+          title={
+            <Typography variant="h6">
+              {work.company}
+              <Link
+                href={work.link}
+                target="_blank"
+                color="grey.dark"
+                paddingLeft={2}
+                fontSize={2}
+              >
+                <LaunchIcon />
+              </Link>
+            </Typography>
+          }
+        />
+
+        <CardContent>
+            <>
+              <Typography variant="h6">{work.role}</Typography>
+              <Typography
+                variant="h6"
+                fontWeight={700}
+                fontSize={12}
+                color="grey.main"
+              >
+                {work.startDate} -{" "}
+                {work.endDate ? work.endDate : "Present"}
+              </Typography>
+              <WorkList>
+        {work.workDone.map((point, index) => (
+          <WorkListItem
+            key={`experience-point-${index}`}
+          >
+            {point}
+          </WorkListItem>
+        ))}
+      </WorkList>
+            </>
+          
+        </CardContent>
+      </Box>
+    </VerticalTimelineElement>
+  );
+};
+
 const Work = () => {
-  const [works,setWorks]=useState(null)
-  useEffect(()=>{
+  const [works, setWorks] = useState(null);
+  useEffect(() => {
     const query = '*[_type=="works"]';
-    client.fetch(query).then(data=>{
-      console.log(data)
-      setWorks(data)
-    })
-  },[])
+    client.fetch(query).then((data) => {
+      console.log(data);
+      setWorks(data);
+    });
+  }, []);
   return (
     <div id="#work">
-      <motion.div whileInView={{opacity:[0,1],y:[100,0]}} transition={{duration:0.4}}>
-      <Typography
-        variant="h3"
-        textAlign="center"
-        fontWeight={700}
-        marginBottom={5}
-        marginTop={10}
+      <motion.div
+        whileInView={{ opacity: [0, 1], y: [100, 0] }}
+        transition={{ duration: 0.4 }}
       >
-        Work
-      </Typography>
-        <Box justifyContent="center" display="flex" flexDirection="column" alignItems="center">
-        {works?.map((work,index) => (
-              <motion.div whileInView={{opacity:[0,1],y:[100,0]}} transition={{duration:0.4 + index/6}}>
-              <Card hover sx={{maxWidth:600,marginBottom:4,cursor:"pointer"}}>
-                <CardHeader
-                  avatar={<img src={urlFor(work.logo)} style={{ width: 70 }} />}
-                  title={
-                    <Typography variant="h6">{work.company}<Link href={work.link} target="_blank" color="grey.dark" paddingLeft={2} fontSize={2}><LaunchIcon/></Link> </Typography>
-                  }
-                />
-               
-                <CardContent>
-                  {work.works.map((workRole) => (
-                    <>
-                     <Typography variant="h6">{workRole.role}</Typography>
-                     <Typography variant="h6" fontWeight={700} fontSize={12} color="grey.main">{workRole.startDate} - {workRole.endDate? workRole.endDate : "Present"}</Typography>
-                     <Typography variant="p">{workRole.desc}</Typography>
-                     <br></br>
-                     <br></br>
-                     </>
-                  ))}
-                </CardContent>
-              </Card>
-              </motion.div>
-        ))}
+        <Typography
+          variant="h3"
+          textAlign="center"
+          fontWeight={700}
+          marginBottom={5}
+          marginTop={10}
+        >
+          Work
+        </Typography>
+        <Box
+          justifyContent="center"
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          sx={{
+            padding:{
+              md:2,
+              xs:1
+            }
+          }}
+        >
+          <VerticalTimeline>
+            {works?.map((work, index) => (
+              <WorkCard key={`work-${index}`} work={work} />
+            ))}
+          </VerticalTimeline>
         </Box>
-        </motion.div>
+      </motion.div>
     </div>
   );
 };
