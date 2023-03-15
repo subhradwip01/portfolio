bba7bb155c2baaa395c6a5d9ba5937fa175be99f
subhradwi01 <subhradwi01@gmail.com>
2023-03-15 18:34:11 +0530

    Work schema update

--- backend/schemas/experience.js
+++ backend/schemas/experience.js
@@ -1,66 +1,64 @@
-export default{
-    name:'works',
-    title:'Works',
-    type:'document',
-    fields:[
-        {
-           name:'company',
-           title:'Company',
-           type:"string",
+export default {
+    name: "works",
+    title: "Works",
+    type: "document",
+    fields: [
+      {
+        name: "company",
+        title: "Company",
+        type: "string",
+      },
+      {
+        name: "logo",
+        title: "Logo",
+        type: "image",
+        options: {
+          hotspot: true,
         },
-        {
-            name:'logo',
-            title:"Logo",
-            type:"image",
-            options:{
-                hotspot:true
-            }
+      },
+      {
+        name: "link",
+        title: "Link",
+        type: "string",
+      },
+      {
+        name: "role",
+        title: "Role",
+        type: "string",
+      },
+      {
+        name: "startDate",
+        title: "startDate",
+        type: "date",
+        options: {
+          dateFormat: "YYYY-MM",
+          calendarTodayLabel: "Today",
         },
-        {
-            name:'link',
-            title:'Link',
-            type:'string',
+      },
+  
+      {
+        name: "endDate",
+        title: "endtDate",
+        type: "date",
+        options: {
+          dateFormat: "YYYY-MM",
+          calendarTodayLabel: "Today",
         },
-        {
-            name:'works',
-            title:'Works',
-            type:"array",
-            of:[{
-                name:'workExperience',
-                title:'Work Experience',
-                type:'document',
-                fields:[
-                       {
-                        name:'role',
-                           title:'Role',
-                           type:'string'
-                        },
-                        {
-                            name:'desc',
-                            title:'Desc',
-                            type:'string'
-                        },
-                        {
-                            name:'startDate',
-                            title:'startDate',
-                            type:'date',
-                            options: {
-                                dateFormat: 'YYYY-MM',
-                                calendarTodayLabel: 'Today'
-                            }
-                        },
-                        {
-                            name:'endDate',
-                            title:'endtDate',
-                            type:'date',
-                            options: {
-                                dateFormat: 'YYYY-MM',
-                                calendarTodayLabel: 'Today'
-                            }
-                        }
-                ]
-            }]
-        }
-    ]
-
-}
+      },
+  
+      {
+        name: "workDone",
+  
+        title: "WorkDone",
+        type: "array",
+        of: [
+          {
+            name: "work",
+            title: "Work",
+            type: "string",
+          },
+        ],
+      },
+    ],
+  };
+  
